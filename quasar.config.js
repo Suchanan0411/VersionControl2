805284bd85e7e54ba1ec8c22fea99eae4c0a543e
Suchanan0411 <suchanan.8864@gmail.com>
2025-10-03 14:00:48 +0700

    edited quasar

--- quasar.config.js
+++ quasar.config.js
@@ -37,7 +37,7 @@ export default defineConfig((ctx) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
       publicPath: '/VersionControl2/',   // ✅ แก้ตรงนี้
-      vueRouterMode: 'hash' // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // webpackTranspile: false,
 
